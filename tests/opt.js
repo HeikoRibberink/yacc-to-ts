3dbbee4aa67bf56946c58691a5ceeecae9615c9c
HeikoRibberink <heiko.ribberink@deblijdeaankomst.nl>
2025-03-11 14:37:44 +0100

    Added comments on improvements

--- tests/opt.js
+++ tests/opt.js
@@ -1,2 +1,2 @@
 empty_a: ($) => $.a,
-use_a: ($) => repeat1(choice($.empty_a,$.empty_a,seq($.empty_a,$.empty_a,),)),
+use_a: ($) => seq(repeat(choice($.empty_a,seq($.empty_a,$.empty_a,),)),),
--- tests/rec-null.js
+++ tests/rec-null.js
@@ -1,3 +1,9 @@
-eimports: ($) => seq(repeat(seq($.IMPORT_EXTERN,$.externimpbody,$.semis1,)),choice(seq($.IMPORT_EXTERN,$.externimpbody,$.semis1,),$.declarations,),),
-declarations: ($) => seq(repeat(seq($.fixitydecl,$.semis1,)),choice(seq($.fixitydecl,$.semis1,),$.topdecls,),),
-topdecls: ($) => $.topdecls1,
+rules = {
+  eimports: ($) =>
+    seq(
+      repeat(seq($.IMPORT_EXTERN, $.externimpbody, $.semis1)),
+      choice(seq($.IMPORT_EXTERN, $.externimpbody, $.semis1), $.declarations),
+    ),
+  declarations: ($) => seq(repeat(seq($.fixitydecl, $.semis1)), $.topdecls),
+  topdecls: ($) => $.topdecls1,
+};
